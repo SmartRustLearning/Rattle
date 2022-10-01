c95ba0e0ef22669bf26a18472d221007c034cd6e
mikail-sw4 <mikail-yagci@outlook.com>
2022-10-02 01:25:49 +0200

    Websocket getMatchID request, vue updated - axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false;
 
