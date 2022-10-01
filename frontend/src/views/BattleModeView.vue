d61d0b439f0177b556931e39ce2f8198b44bda74
mikail-sw4 <mikail-yagci@outlook.com>
2022-10-01 16:23:25 +0200

    frontend changed to allow routers

---
+++ frontend/src/views/BattleModeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <p>aasas</p>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/LandingPageView.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <h1>hello and welcome</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore atque et ea.</p>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
