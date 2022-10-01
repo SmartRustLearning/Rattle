d4d047ea65e6219c0fbc15ce9a02f1f280c2d806
mikail-sw4 <mikail-yagci@outlook.com>
2022-10-01 22:39:23 +0200

    starting modal added and landingPage changed

--- frontend/src/views/LandingPageView.vue
+++ frontend/src/views/LandingPageView.vue
@@ -1,21 +1,33 @@
 <template>
   <main>
     <div class="content">
-      <h1>READY FOR <br/>BATTLE</h1>
+      <h1>READY FOR <br/>BATTLE!</h1>
       <img src="../assets/angryRust.png" alt="">
     </div>
     <div class="buttonLayer">
-      <router-link to ="/battle">
-        <button class="button" style="vertical-align:middle">start</button>
-      </router-link>
+        <button class="button" style="vertical-align:middle; font-size:2rem; font-weight:bold;" @click="openModal()">start</button>
     </div>
-    
+    <div @click="modal=false" v-if="modal" style="width:100vw; height:100vh;">
+      <div class="modalBackground" @click.stop>
+        <div class="modal">
+          <h2 style="margin-top:80px; font-size:2.4rem;">Game is loading...</h2>
+          <div style="display: flex;"><img src="../assets/Crab-unscreen.gif" alt=""></div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
 export default {
-
+  data: () => ({
+    modal: false,
+  }),
+  methods: {
+    openModal(){
+        this.modal = !this.modal;
+    }
+  },
 }
 </script>
 
@@ -39,12 +51,48 @@ h1{
   margin: 0;
 }
 
-img{
-  width: 400px;
+.content img{
+  width: 500px;
   height: fit-content;
+  position: relative;
+  top: 50px;
 }
 
 .buttonLayer{
   text-align: center;
+  display: flex;
+}
+
+.buttonLayer .button{
+  position: relative;
+  left: 200px;
+  bottom: 50px;
+}
+
+.modalBackground{
+  background: #00000079;
+  position: absolute;
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.modal{
+  width: 600px;
+  height: 400px;
+  border-radius: 12px;
+  background: white;
+}
+
+.modal img{
+  width: 500px;
+  position: relative;
+  top: -75px;
+  right: -120px;
 }
 </style>
