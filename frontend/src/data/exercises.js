88fd0d7f970345ca9674da8d736a5f98e2ef4e8a
André Kovac <me@andrekovac.com>
2022-10-02 09:43:18 +0200

    add second exercise to frontend

--- frontend/src/data/exercises.js
+++ frontend/src/data/exercises.js
@@ -2,7 +2,7 @@ export const exercises = [
   {
     id: 1,
     duration: 30, // seconds
-    problem: `Make sure it's printing the String "Number 2"`,
+    problem: `This code has a syntax error. Make sure it's printing the String "Number 2"`,
     starterCode: `fn main() {
     const x = 1;
     println!("Number {}", x);
@@ -12,5 +12,31 @@ export const exercises = [
   println!("Number {}", x);
 }`,
     explanation: `Watch for the little things. Bust the compiler. It'll always beat you to it. Constants are not to be mistreated.In Rust the "const" keyword requires an explicit type!`,
+  },
+  {
+    id: 2,
+    duration: 60,
+    problem:
+      'The Rust compiler will show you this error: "cannot borrow `x` as mutable more than once at a time". Make the code compile only by reordering the lines, but without adding, changing or removing any of them',
+    starterCode: `fn main() {
+  let mut x = 100;
+  let y = &mut x;
+  let z = &mut x;
+  *y += 100;
+  *z += 1000;
+  assert_eq!(x, 1200);
+}
+    `,
+    solutionCode: `fn main() {
+  let mut x = 100;
+  let y = &mut x;
+  *y += 100;
+  let z = &mut x;
+  *z += 1000;
+  assert_eq!(x, 1200);
+}`,
+    explanation: `Carefully reason about the range in which each mutable reference is in
+  scope. Does it help to update the value of referent (x) immediately after
+  the mutable reference is taken?`,
   },
 ];
